c3507b54ec5f8333653d6fa261278b25be79dbe7
Ilya <ignislumenhoros@yandex.ru>
2026-06-07 17:44:39 +0300

    KS-3-2:concatenation is a change of last name try

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,12 @@ package org.example
 fun main() {
     val firstName = "Татьяна"
     val patronymic = "Сергеевна"
-    var LastName = "Андреева"
+    var lastName = "Андреева"
     var age = 20
-    println("$LastName $firstName $patronymic $age")
-    LastName = "Сидорова"
+    println("[$lastName $firstName $patronymic, $age]")
+    lastName = "Сидорова"
     age = 22
-    println("$LastName $firstName $patronymic $age")
+    println("[$lastName $firstName $patronymic, $age]")
 
 
 }
