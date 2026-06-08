b36210c28a4fe7b843d3c13b35979d11c651d043
Ilya <ignislumenhoros@yandex.ru>
2026-06-08 21:55:19 +0300

    KS-3-5: split

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,18 @@
 package org.example
 
 fun main() {
-    fun main() {
-        val input = "D2-D4;0"
+    val input = "D2-D4;0"
 
-        // Разделяем строку по точке с запятой — получаем ход и номер
-        val (move, turnNumber) = input.split(";")
-        // Разделяем ход по дефису — получаем исходную и целевую клетку
-        val (from, to) = move.split("-")
-        // Преобразуем номер хода в число
+    // Разделяем строку по точке с запятой — получаем ход и номер
+    val (move, turnNumber) = input.split(";")
+    // Разделяем ход по дефису — получаем исходную и целевую клетку
+    val (from, to) = move.split("-")
+    // Преобразуем номер хода в число
 
 
-        println("Откуда: $from")
-        println("Куда: $to")
-        println("Номер хода: $turnNumber")
+    println("Откуда: $from")
+    println("Куда: $to")
+    println("Номер хода: $turnNumber")
     }
 
 
-}
