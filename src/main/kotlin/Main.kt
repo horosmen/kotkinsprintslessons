b17fe67385e2d0d3605d27a2d563b99763676dd4
Ilya <ignislumenhoros@yandex.ru>
2026-06-08 21:51:00 +0300

    KS-3-5: split

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,20 @@
 package org.example
 
 fun main() {
-// Шаг 1. Инициализация переменных для первого хода
-    var from: String = "E2"     // Начало позиция
-    var to: String = "E4"       // Конечная позиция
-    var moveNumber: Int = 1     // Номер хода
+    fun main() {
+        val input = "D2-D4;0"
 
-// Шаг 2 формирование строки хода
-    val moveString: String = "$from-$to;$moveNumber"
-    println(moveString)
+        // Разделяем строку по точке с запятой — получаем ход и номер
+        val (move, turnNumber) = input.split(";")
+        // Разделяем ход по дефису — получаем исходную и целевую клетку
+        val (from, to) = move.split("-")
+        // Преобразуем номер хода в число
 
-// Шаг 3 Следующий ход
-    from = "D2"
-    to = "D3"
-    moveNumber++
 
-// Шаг 4 формирование строки хода
-    val nextMoveString = "$from-$to;$moveNumber"
-    println(nextMoveString)
+        println("Откуда: $from")
+        println("Куда: $to")
+        println("Номер хода: $turnNumber")
+    }
+
 
 }
