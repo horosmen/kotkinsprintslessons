f6b5545a5c678121a0c0f13ae9200b86e7cd35aa
Ilya <ignislumenhoros@yandex.ru>
2026-06-08 17:10:16 +0300

    recode

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,19 +4,19 @@ fun main() {
 // Шаг 1. Инициализация переменных для первого хода
     var from: String = "E2"     // Начало позиция
     var to: String = "E4"       // Конечная позиция
-    var moveNumber: Int = 1     // Номер хода
+    var moveNumbe: Int = 1     // Номер хода
 
 // Шаг 2 формирование строки хода
-    val moveString: String = "$from-$to;$moveNumber"
+    val moveString: String = "$from-$to;$moveNumbe"
     println(moveString)
 
 // Шаг 3 Следующий ход
     from = "D2"
     to = "D3"
-    moveNumber++
+    moveNumbe++
 
 // Шаг 4 формирование строки хода
-    val nextMoveString = "$from-$to;$moveNumber"
+    val nextMoveString = "$from-$to;$moveNumbe"
     println(nextMoveString)
 
 }
