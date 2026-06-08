066e17e557d5fe038ff1ff675d2c6a7c29e70483
Ilya <ignislumenhoros@yandex.ru>
2026-06-08 17:14:01 +0300

    KS-3-4: recorder

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,19 +4,19 @@ fun main() {
 // Шаг 1. Инициализация переменных для первого хода
     var from: String = "E2"     // Начало позиция
     var to: String = "E4"       // Конечная позиция
-    var moveNumbe: Int = 1     // Номер хода
+    var moveNumber: Int = 1     // Номер хода
 
 // Шаг 2 формирование строки хода
-    val moveString: String = "$from-$to;$moveNumbe"
+    val moveString: String = "$from-$to;$moveNumber"
     println(moveString)
 
 // Шаг 3 Следующий ход
     from = "D2"
     to = "D3"
-    moveNumbe++
+    moveNumber++
 
 // Шаг 4 формирование строки хода
-    val nextMoveString = "$from-$to;$moveNumbe"
+    val nextMoveString = "$from-$to;$moveNumber"
     println(nextMoveString)
 
 }
