26182a7696c99d3fa52a8bae9189ad36ef33b474
Ilya <ignislumenhoros@yandex.ru>
2026-06-09 12:09:43 +0300

    KS-4-1: Comparison - table reservations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 package org.example
 
+const val TABLE_IN_RESTORANT = 13
 fun main() {
 
-
+    val todayBooked = 13
+    val tomorrowBooked = 9
+    val isAvailabilityOfTablesForToday = todayBooked < TABLE_IN_RESTORANT
+    val isAvailabilityOfTablesForTomorrow = tomorrowBooked < TABLE_IN_RESTORANT
+    println("Доступность столиков на сегодня: $isAvailabilityOfTablesForToday\nДоступность столиков на завтра: $isAvailabilityOfTablesForTomorrow")
 }
