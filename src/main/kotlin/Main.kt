be598ed555dd808d25157beee280725d20d7790a
Ilya <ignislumenhoros@yandex.ru>
2026-06-10 16:13:11 +0300

    KS-4-3: Logic && and || - Plant Monitoring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 package org.example
 
 fun main() {
-
+    val weatherToDay: Boolean = true
+    val tentOpen: Boolean = true
+    val airHumidity: Int = 20
+    val season: String = "winter"
+    val favorableConditions = weatherToDay && tentOpen && airHumidity == 20 && season != "winter"
+    println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 
 }
