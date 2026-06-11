a9a499ffd5033fb6940ba397802b8e6b9bfbbe97
Ilya <ignislumenhoros@yandex.ru>
2026-06-11 18:10:37 +0300

    KS-4-4: Inversion ! — training program

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,36 @@
 package org.example
 
 fun main() {
+    val dayNomber = 5
+    var (isHands, isPress) = Pair(true, true)
+    var (isLegs, isBack) = Pair(true, true)
+    if (dayNomber % 2 == 0) {
+        isHands = !isHands
+        isPress = !isPress
+        println(
+            """
+        Упражнения для рук:$isHands
+
+        Упражнения для ног: $isLegs
+
+        Упражнения для спины: $isBack
+
+        Упражнения для пресса: $isPress"""
+        )
+    } else {
+        isLegs = !isLegs
+        isBack = !isBack
+        println(
+            """
+        Упражнения для рук:$isHands
+
+        Упражнения для ног: $isLegs
+
+        Упражнения для спины: $isBack
+
+        Упражнения для пресса: $isPress"""
+        )
+    }
 
 
 }
