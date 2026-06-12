c7cb506d3f2c8cc99aaaf456efb8b86786c6c5bc
Ilya <ignislumenhoros@yandex.ru>
2026-06-12 10:48:53 +0300

    KS-4-5: Compound Conditions - Ship Departure_new

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,14 +5,14 @@ const val CREW_MAX = 70
 const val PROVISION_MIN = 50
 
 fun main() {
-    val hasDamage: Boolean = readln().toBoolean()
-    val isСrew: Int = readln().toInt()
-    val isFoodBoxes: Int = readln().toInt()
+    val isDamage: Boolean = readln().toBoolean()
+    val crew: Int = readln().toInt()
+    val foodBoxes: Int = readln().toInt()
     val isFavorableWeather = readln().toBoolean()
 
-    val canDepart = (!hasDamage && isСrew in CREW_MIN..CREW_MAX && isFoodBoxes > PROVISION_MIN) ||
-            (hasDamage && isСrew == 70 && isFavorableWeather && isFoodBoxes >= PROVISION_MIN)
+    val isCanDepart = (!isDamage && crew in CREW_MIN..CREW_MAX && foodBoxes > PROVISION_MIN) ||
+            (isDamage && crew == 70 && isFavorableWeather && foodBoxes >= PROVISION_MIN)
 
-    println(if (canDepart) "Корабль может отправиться в плавание" else "Корабль не может отправиться в плавание")
+    println(if (isCanDepart) "Корабль может отправиться в плавание" else "Корабль не может отправиться в плавание")
 
 }
