00fcdacca7dad19a2c16ac2090bd262a52350f34
Ilya <ignislumenhoros@yandex.ru>
2026-06-12 10:28:44 +0300

    KS-4-5: Compound Conditions - Ship Departure

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,13 @@
 package org.example
 
 fun main() {
+    val damage: Boolean = readln().toBoolean()
+    val crew: Int = readln().toInt()
+    val foodBoxes: Int = readln().toInt()
+    val favorableWeather = readln().toBoolean()
 
+    val canDepart = (!damage && crew in 55..70 && foodBoxes > 50 && (favorableWeather || !favorableWeather)) || (damage && crew == 70 && favorableWeather && foodBoxes >= 50)
+
+    println(if (canDepart) "Корабль может отправиться в плавание" else "Корабль не может отправиться в плавание")
 
 }
