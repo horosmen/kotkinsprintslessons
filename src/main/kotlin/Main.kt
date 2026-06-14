0b86d69b1e5368c67af02255e9d0400f80ab9b5f
Ilya <ignislumenhoros@yandex.ru>
2026-06-14 21:53:31 +0300

    KS-5-2: if-else - checking for adulthood-new

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,16 +4,14 @@ package org.example
 const val LEGAL_AGE = 18
 fun main() {
     // Получаем текущий год
-    val currentYear: Int = java.time.Year.now().value
+    val currentYear = 2026
     // Запрашиваем у пользователя год рождения
-    print("Введите год рождения: ")
+    println("Введите год рождения: ")
     val birthYear = readln().toInt()
     // Вычисляем возраст пользователя
     val age = currentYear - birthYear
     // Проверяем условие совершеннолетия и выводим соответствующее сообщение
     if (age >= LEGAL_AGE) {
         println("Показать экран со скрытым контентом")
-    } else {
-        println("Доступ к скрытому контенту ограничен: возраст меньше $LEGAL_AGE лет.")
     }
 }
