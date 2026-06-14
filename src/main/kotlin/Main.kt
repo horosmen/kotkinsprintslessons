005db5bbe1935939a3816c2faaafd6b2f93809ba
Ilya <ignislumenhoros@yandex.ru>
2026-06-14 10:34:44 +0300

    KS-5-1: if — authorization against bots-new

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 package org.example
 
 fun main() {
-    val verificationFunction = 5 + 9
+    val randomNumberOne: Int = (0..10).random()
+    val randomNumberTwo: Int = (0..10).random()
+    val verificationNuber = randomNumberOne + randomNumberTwo
+    println("Решите пример: $randomNumberOne + $randomNumberTwo = ")
     val enteredNumber = readln().toInt()
 
-    if (verificationFunction == enteredNumber) {
+    if (verificationNuber == enteredNumber) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещен.")
