f97cfea5aa2eb185066aed0fe9d007f1c35c9d23
ilya <ignislumenhoros@yandex.ru>
2026-06-15 15:36:43 +0300

    KS-5-3: if as an expression - lottery

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 package org.example
 
 fun main() {
-
-
+    val winningNumberOne = 15
+    val winningNumberTwo = 30
+    println("Введите ваше первое число для победы: ")
+    val checkNumberOne = readln().toInt()
+    println("Введите ваше второе число для победы: ")
+    val checkNumberTwo = readln().toInt()
+    if (winningNumberOne == checkNumberOne && winningNumberTwo == checkNumberTwo)
+        {println("Поздравляем! Вы выиграли главный приз!")}
+    else  if (winningNumberOne == checkNumberOne || winningNumberTwo == checkNumberTwo)
+        { println("Вы выиграли утешительный приз!")}
+    else {
+        println("Неудача!")
+    }
 }
