06505856aa57f4d3637a111f939ac9d9367102c2
ilya <ignislumenhoros@yandex.ru>
2026-06-18 20:06:04 +0300

    KS-6-2: While Counter - Console Timer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,15 @@ package org.example
 
 fun main() {
     println("Пожалуйста задайте таймеру количество секунд: ")
-    var count = readln().toInt()
+    var count = readlnOrNull()?.toIntOrNull()
+    if (count== null){
+        println("введите число")
+    return }
     val totalSeconds = count
-    while (count > 0) {
+    while (count > 0 && count!= null) {
         Thread.sleep(1000)   // пауза 1 секунда (1000 мс)
         count--
-    }
+        }
     println("Прошло: $totalSeconds секунд")
 }
+
