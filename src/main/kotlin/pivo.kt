85aba2a5c8a5b76b64b18c0389254877c40b050b
ilya <ignislumenhoros@yandex.ru>
2026-06-18 19:43:03 +0300

    KS-6-2: While Counter - Console Timer

---
+++ src/main/kotlin/pivo.kt
@@ -0,0 +1,16 @@
+﻿package org.example
+
+fun main() {
+    println("Пожалуйста зарегистрируйтесь Введите логин: ")
+    val newLoginUser = readln()
+    println("Введите пароль: ")
+    val newPasswordUser = readln()
+    do {
+        println("Введите зарегистрированный логин и пароль чтобы войти в программу:")
+        val login = readln()
+        val password = readln()
+        if (login == newLoginUser && password == newPasswordUser) {
+            println("Авторизация прошла успешно")
+        }
+    } while (login != newLoginUser || password != newPasswordUser)
+}
