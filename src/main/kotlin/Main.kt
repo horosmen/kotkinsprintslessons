41fe14c3c11c082606514be5ecab53cb86fbb78f
ilya <ignislumenhoros@yandex.ru>
2026-06-20 12:00:33 +0300

    KS-6-5: continue — captcha with attempts

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,21 @@ package org.example
 
 fun main() {
 
-
+    var attempt = 1
+    while (attempt <= 3) {
+        val randomNumberOne: Int = (1..9).random()
+        val randomNumberTwo: Int = (1..9).random()
+        val correctAnswer = randomNumberOne + randomNumberTwo
+        println("Решите пример: $randomNumberOne + $randomNumberTwo = ")
+        val enteredNumber = readln().toInt()
+        if (correctAnswer == enteredNumber) {
+            println("Добро пожаловать!")
+            break
+        }
+        attempt++
+        continue
+    }
+    if (attempt > 3) {
+        println("Доступ запрещен.")
+    }
 }
