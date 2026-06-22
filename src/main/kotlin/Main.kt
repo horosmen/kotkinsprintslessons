9560dbd12fd60cdcb48a57114ca7f16daf7f3bb0
ilya <ignislumenhoros@yandex.ru>
2026-06-22 20:44:40 +0300

    KS-7-3: until - even numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
-package org.example
-
 fun main() {
-
-
+    println("Введите число")
+    val userInput = readln().toIntOrNull()
+    val userNumber = userInput
+    if (userInput != null) {
+        for (i in 0..userNumber step 2) {
+            print("$i ")
+        }
+    }
 }
