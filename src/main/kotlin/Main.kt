175781fd6d2da496935023ce2e815da971a7aff9
ilya <ignislumenhoros@yandex.ru>
2026-06-22 20:41:01 +0300

    KS-7-2: downTo — SMS authorization

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,23 @@
-package org.example
+import kotlin.random.Random
 
 fun main() {
+    while (true) {
+        val authCode = Random.nextInt(1000, 10000)
+        println("Ваш код авторизации: $authCode")
 
+        print("Введите код для авторизации: ")
+        val userInput = readln()
+        val userCode = userInput.toIntOrNull() // Возвращает null при ошибке
 
+        if (userCode != null && userInput.length == 4) {
+            if (userCode == authCode) {
+                println("Добро пожаловать! Авторизация успешна.")
+                break
+            } else {
+                println("Неверный код. Попробуйте снова.")
+            }
+        } else {
+            println("Некорректный ввод. Пожалуйста, введите 4‑значный числовой код.")
+        }
+    }
 }
