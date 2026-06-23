ffe3ecb5879514d4c026db9832c5fe23b8c632a1
ilya <ignislumenhoros@yandex.ru>
2026-06-23 20:45:55 +0300

    KS-7-4: for the collection - timer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 package org.example
 
 fun main() {
+    println("Задайте количество секунд: ")
+    val userInputSeconds = readln().toIntOrNull()
+    if (userInputSeconds != null) {
+        for (i in userInputSeconds downTo 1) {
+            println("Осталось: $i секунд")
+            Thread.sleep(1000)
+        }
+    } else {
+        println("Ошибка вы ввели не число.")
+        return
+    }
 
-
+    println("Время вышло")
 }
