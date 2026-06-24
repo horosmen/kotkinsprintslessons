59be87c24a2bee571a888320096405eed04d12d7
ilya <ignislumenhoros@yandex.ru>
2026-06-24 20:18:54 +0300

    KS-7-5: forEachIndexed — complex password

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,26 @@ package org.example
 
 fun main() {
 
+    val uppercases = ('A'..'Z').toList()
+    val lowercase = ('a'..'z').toList()
+    val numbers = ('0'..'9').toList()
+    println("Задайте длинну пароля: ")
+    val userInput = readln().toIntOrNull()
+    if (userInput != null && userInput >= 6) {
+        val passwordChars = mutableListOf<Char>()
+        passwordChars.add(uppercases.random())
+        passwordChars.add(lowercase.random())
+        passwordChars.add(numbers.random())
+        val allSymbols = uppercases + lowercase + numbers
+
+        repeat(userInput - 3) {
+            passwordChars.add(allSymbols.random())
+        }
+
+        passwordChars.shuffle()
+        println(passwordChars.joinToString(""))
+    }
+
 
 }
+
