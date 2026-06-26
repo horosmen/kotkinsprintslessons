2750774617045a27ecc5d46a682e4e2d405b45bd
ilya <ignislumenhoros@yandex.ru>
2026-06-26 21:07:45 +0300

    KS-8-1: arrayOf — view statistics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 package org.example
 
 fun main() {
-
+    val days = IntArray(7)
+    for (i in days.indices) {
+        println("Введите сколько вы посмотрели рекламы сегодня: ")
+        days[i] = readln().toInt()
+    }
+    println("На этой неделе вы посмотрели: ${days.sum()}")
 
 }
