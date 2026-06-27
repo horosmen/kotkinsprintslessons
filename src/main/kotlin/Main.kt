4e0ac81e036b1795e14eff1aafada2d830459c99
ilya <ignislumenhoros@yandex.ru>
2026-06-27 10:54:22 +0300

    KS-8-3: Searching an array — a loop-free recipe

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
-package org.example
-
 fun main() {
+    val recipesFriedEggs = arrayOf("Яйцо", "Соль", "Перец", "Лук", "Помидор", "Колбаса")
+    println("Какой ингредиент вы хотите найти?: ")
+    val request = readln()
 
-
+    if (request in recipesFriedEggs) {
+        println("Ингредиент $request в рецепте есть")
+    } else {
+        println("Такого ингредиента в рецепте нет")
+    }
 }
