e7feac32c504c7eb55f24f311bb5765c2759a89e
ilya <ignislumenhoros@yandex.ru>
2026-06-28 11:19:50 +0300

    KS-8-5: Array size — recipe constructor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
 package org.example
 
 fun main() {
-
-
+    println("Введите количество ингредиентов:")
+    val count = readln().toInt()
+    val ingredients = Array(count){""}
+    for (i in 0..< count){
+        println("Введите ингредиент: ${i+1}")
+        ingredients[i] = readln()
+    }
+    println(ingredients.joinToString(","))
 }
