a40dfe53319b4f995125b418aa6559134642fff0
ilya <ignislumenhoros@yandex.ru>
2026-06-30 20:33:39 +0300

    KS-9-1: listOf — list of ingredients

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
-package org.example
-
 fun main() {
+    val ingridients = listOf("Яйца", "Масло", "Лук", "Пиво")
 
+    println("В рецепте есть следующие ингредиенты: [${ingridients.joinToString(", ")}]")
 
+    var i = 1
+    ingridients.forEach { it ->
+        println("$i. $it")
+        i++
+    }
 }
