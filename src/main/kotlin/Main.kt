3d980085770b983570688bce7e054da312241289
ilya <ignislumenhoros@yandex.ru>
2026-07-03 19:52:51 +0300

    KS-9-3: += operators — portion calculation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,23 @@
 fun main() {
-    val ingredients = mutableListOf("Яйца", "Масло", "Пиво")
+    val ingredients = mutableListOf(2, 50, 15)
 
-    println("В рецепте есть базовые ингредиенты: [${ingredients.joinToString(", ")}]")
-    println("Желаете добавить ещё?")
-
-    var ingredient = readlnOrNull()
-    if (ingredient == null || ingredient.equals("нет", ignoreCase = true)) {
+    val numberOfServings = readln().toIntOrNull() ?: run {
+        println("Введите число")
         return
-    } else if (ingredient.equals("да", ignoreCase = true)) {
-        println("Какой ингредиент вы хотите добавить?")
-        var ingredient = readln()
-        ingredients.add(ingredient)
-        println("Теперь в рецепте есть следующие ингредиенты:[${ingredients.joinToString(", ")}]")
     }
 
+    if (numberOfServings <= 0) {
+        println("Введите число больше 0")
+        return
+    }
+
+    for (i in ingredients.indices) {
+        ingredients[i] = ingredients[i] * numberOfServings
+    }
+
+    println(
+        "На $numberOfServings порций вам понадобится: " +
+                "Яиц – ${ingredients[0]}, молока – ${ingredients[1]}, " +
+                "сливочного масла – ${ingredients[2]}"
+    )
 }
