2d66e9475fe084fad90a92aba60643e584505cac
ilya <ignislumenhoros@yandex.ru>
2026-07-03 20:38:15 +0300

    KS-9-4: find and filter — searching for an ingredient

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
-package org.example
-
 fun main() {
-
-
+    println("Hi friend, enter 5 ingredients separated by commas.")
+    val ingredients = readln().split(", ")
+    if (ingredients.size != 5) {
+        println("Oh, damn, sorry, but I need 5 ingredients. (introduced: ${ingredients.size})")
+        return
+    }
+    println(ingredients.sorted())
 }
