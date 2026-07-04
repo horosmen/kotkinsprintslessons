8d41908cddcd3c811f2d363ea40d212a43e44325
ilya <ignislumenhoros@yandex.ru>
2026-07-04 11:56:39 +0300

    KS-9-5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,12 @@
 fun main() {
-    println("Hi friend, enter 5 ingredients separated by commas.")
-    val ingredients = readln().split(", ")
-    if (ingredients.size != 5) {
-        println("Oh, damn, sorry, but I need 5 ingredients. (introduced: ${ingredients.size})")
-        return
+    val ingredients = mutableListOf<String>()
+    println("Hi friend, enter 5 ingredients.")
+    while (ingredients.size < 5) {
+        val input = readln()
+        if (!ingredients.contains(input)) ingredients.add(input)
+
     }
-    println(ingredients.sorted())
+    ingredients.sort()
+    ingredients[0] = ingredients[0].replaceFirstChar { it.uppercase() }
+    println(ingredients.joinToString(", "))
 }
