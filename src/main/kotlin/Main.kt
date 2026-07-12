f5125c7efe6f95734b4109f839fa2a59adc9df42
ilya <ignislumenhoros@yandex.ru>
2026-07-12 15:56:18 +0300

    KS-10-1: Functions — rolling the die

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,22 @@
-package org.example
-
 fun main() {
 
+    val resultHuman = rollDice()
+    val resultComputer = rollDice()
+    println("Human rolled a ${resultHuman}.")
+
+    println("Computer rolled a ${resultComputer}.")
+
+    if (resultHuman > resultComputer) {
+        println("Humanity won.")
+    } else if (resultHuman < resultComputer) {
+        println("The machine won.")
+    } else {
+        println("Friendship won.")
+    }
 
 }
+
+fun rollDice(): Int = (1..6).random()
+
+
+
