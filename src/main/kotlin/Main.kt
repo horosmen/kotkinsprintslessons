991077b72cfc11001682d1e2aedfe9f732682257
ilya <ignislumenhoros@yandex.ru>
2026-07-13 20:30:56 +0300

    KS-10-2 Parameters — login validation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,15 @@
-package org.example
-
 fun main() {
+    println("Введите ваш логин и пароль")
+    val isValid = validateLogin(login = readln(), password = readln())
+    if (isValid == false) {
+        println("Логин или пароль недостаточно длинные")
+    } else {
+        println("Привет Юзер!")
+    }
+
+}
 
 
+fun validateLogin(login: String, password: String): Boolean {
+    return login.length >= 4 && password.length >= 4
 }
