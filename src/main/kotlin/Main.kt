f6451b1ec2efa4c740831edae2b35f01ed595553
ilya <ignislumenhoros@yandex.ru>
2026-07-15 21:06:21 +0300

    KS-10-4 Named arguments — improved block

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,42 @@
 fun main() {
+    println("Добро пожаловать в игру «Бросок костей»!")
+
+    var humanWins = 0
+    var answer: String
+
+    do {
+        if (playRound()) {
+            humanWins++
+        }
+
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+        answer = readln()
+
+    } while (answer == "Да")
+
+    println("Человек выиграл $humanWins партий.")
+}
+
+fun rollDice(sides: Int = 6): Int = (1..sides).random()
+
+fun playRound(): Boolean {
+
     println("Бросок человека")
-
-    val resultHuman = rollDice()
-
-    println("Человек бросил:  ${resultHuman}.")
+    val resultHuman = rollDice(sides = 6)
+    println("Человек бросил: $resultHuman")
 
     println("Бросок компьютера")
-
-    val resultComputer = rollDice()
-
-    println("Компьютер бросил:  ${resultComputer}.")
+    val resultComputer = rollDice(sides = 6)
+    println("Компьютер бросил: $resultComputer")
 
     if (resultHuman > resultComputer) {
         println("Победило человечество")
+        return true
     } else if (resultHuman < resultComputer) {
         println("Победила машина")
+        return false
     } else {
         println("Победила дружба")
+        return false
     }
-
 }
-
-fun rollDice(): Int = (1..6).random()
-
-
-
