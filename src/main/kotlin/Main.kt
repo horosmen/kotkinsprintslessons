58f8ed768d481cc2d2d15a3af30565f073f279b2
ilya <ignislumenhoros@yandex.ru>
2026-07-16 20:59:12 +0300

    KS-10-5 Expression body

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,45 @@
-package org.example
+const val LOGIN = "admin"
+const val PASSWORD = "12345"
+
+val cart = listOf("Наушники", "Ноутбук", "Смарт-часы")
+val userLogin = "admin"
+val userPassword = "12345"
+private const val TOKEN_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
 fun main() {
+    val token = authorize(userLogin, userPassword)
+    val basket = getCart(token)
+    if (basket == null) {
+        println("Авторизация не удалась")
+    } else {
+        println("Товары в корзине:")
 
-
+        for (item in basket) {
+            println(item)
+        }
+    }
 }
+
+
+fun authorize(login: String, password: String): String? {
+    if (login == LOGIN && password == PASSWORD) {
+        var token = ""
+        repeat(32) {
+            token += TOKEN_CHARS.random()
+
+        }
+        return token
+    } else {
+        return null
+    }
+}
+
+fun getCart(token: String?): List<String>? {
+
+    if (token == null) {
+        return null
+    }
+
+    return cart
+}
+
