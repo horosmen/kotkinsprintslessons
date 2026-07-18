cf03bb121ffa841486a60dc7669604e52590be68
ilya <ignislumenhoros@yandex.ru>
2026-07-18 19:00:25 +0300

    KS-11-1 Classes — User Model

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,26 @@
-package org.example
+class User(val id: Int) {
+    var login: String = ""
+    var password: String = ""
+    var email: String = ""
+}
+
+fun userOne() {
+    val user = User(id = 1)
+    user.login = "admin"
+    user.password = "admin"
+    user.email = "horos030590@mail.ru"
+   println("Welcome ${user.id}, ${user.login}! you email ${user.email}")
+}
+
+fun userTwo() {
+    val user = User(id = 2)
+    user.login = "admin"
+    user.password = "admin"
+    user.email = "yandex.ru"
+    println("Welcome ${user.id}, ${user.login}! you email ${user.email}")
+}
 
 fun main() {
-
-
+    userOne()
+    userTwo()
 }
