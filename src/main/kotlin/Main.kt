12f92e44e210060f449bf4fc8955f48914183c4b
ilya <ignislumenhoros@yandex.ru>
2026-07-21 19:03:10 +0300

    KS-11-4 Methods — RecipesApp

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,26 @@
-package org.example
+data class Category(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val imageUrl: String?,
+)
 
-fun main() {
+data class Ingredient(
+    val id: Int,
+    val name: String,
+)
 
+data class Recipe(
+    val id: Int,
+    val categoryId: Int,
+    val name: String,
+    val imageUrl: String?,
+    val ingredients: List<RecipeIngredient>,
+)
 
-}
+data class RecipeIngredient(
+    val recipeId: Int,
+    val ingredientId: Int,
+    val amount: Double,
+    val unit: String,
+)
