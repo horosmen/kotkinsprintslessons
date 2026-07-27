24d5ec6941dc7d92eb37885f6fd877d9c2e60c8d
ilya <ignislumenhoros@yandex.ru>
2026-07-27 20:16:37 +0300

    KS-12-2 init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,25 @@
-class WeatherDay {
-    var dayTimeTemperature: Double = 0.0
-    var nightTimeTemperature: Double = 0.0
-    var precipitation: Boolean = false
-    var dayOf: String = ""
+class Weather(var dayTemperature: Double, var nightTemperature: Double, var precipitation: Boolean) {
 
-    fun reportOfWeather(){
-        println("$dayOf Day temperature: $dayTimeTemperature and precipitation: $precipitation")
-        println("$dayOf Night temperature: $nightTimeTemperature and precipitation: $precipitation")
+    init {
+        if (precipitation == false) {
+            println("You don't need an umbrella.")
+        } else {
+            println("You need an umbrella.")
+        }
+
+    }
+
+    fun weatherReport() {
+        println("Day temperature: $dayTemperature, Night temperature: $nightTemperature, precipitation: $precipitation")
     }
 }
 
-fun main() {
-    val weatherDay1 = WeatherDay()
-    weatherDay1.dayOf = "Monday"
-    weatherDay1.dayTimeTemperature = 31.2
-    weatherDay1.nightTimeTemperature = 20.5
-    weatherDay1.precipitation = true
-    weatherDay1.reportOfWeather()
 
-    val weatherDay2 = WeatherDay()
-    weatherDay2.dayOf = "Tuesday"
-    weatherDay2.dayTimeTemperature = 25.2
-    weatherDay2.nightTimeTemperature = 10.2
-    weatherDay2.reportOfWeather()
+fun main() {
+    val weatherDay1 = Weather(31.2, 15.2, true)
+    val weatherDay2 = Weather(41.1, 19.2, false)
+    weatherDay1.weatherReport()
+    weatherDay2.weatherReport()
+
+
 }
