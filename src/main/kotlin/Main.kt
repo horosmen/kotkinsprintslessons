fbe4b5cbb33bb145584d1a19c649ee8637dc0d2d
ilya <ignislumenhoros@yandex.ru>
2026-07-29 20:42:16 +0300

    KS-12-4 require

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,18 +9,14 @@ class Weather(dayKelvin: Int, nightKelvin: Int, precipitation: Boolean) {
         nightTemperature = nightKelvin - 273
         this.precipitation = precipitation
 
-    }
+        require(dayTemperature >= -273 && nightTemperature >= -273) { "TTemperature below absolute zero" }
 
-    fun weatherReport() {
-        println("Day temperature: $dayTemperature ℃ , Night temperature: $nightTemperature ℃ , precipitation:$precipitation")
+        println("Day temperature: $dayTemperature ℃, Night temperature: $nightTemperature ℃, precipitation: $precipitation")
     }
 
 }
 
 fun main() {
     val weatherDay1 = Weather(304, 300, true)
-    val weatherDay2 = Weather(300, 300, false)
-    weatherDay1.weatherReport()
-    weatherDay2.weatherReport()
 
 }
