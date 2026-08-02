50863e4c595f97f4838d19c33a0df105b2e4d8a7
ilya <ignislumenhoros@yandex.ru>
2026-08-02 12:00:33 +0300

    KS-13-2 The ?. operator — safe call

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,9 +7,7 @@ class Directory(val name: String, val phoneNumber: Long, val company: String? = 
 }
 
 fun main() {
-    val directoryAnton = Directory("Ростислав", 89123456789, "Reddit")
     val directoryAnna = Directory("Анна", 890005554442, null)
-    directoryAnton.print()
     directoryAnna.print()
 }
 
