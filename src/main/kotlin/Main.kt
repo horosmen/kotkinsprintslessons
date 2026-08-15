d5d97dd340882c6d0cfb11c332dbbbc3c4f8b576
ilya <ignislumenhoros@yandex.ru>
2026-08-15 10:08:06 +0300

    KS-15-1 interface

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,50 @@
-package org.example
+// Интерфейсы для типов передвижения
+interface Flyable {
+    fun fly()
+}
+
+interface Swimmable {
+    fun swim()
+}
+
+// Классы существ
+class Carp : Swimmable {
+    override fun swim() {
+        println("Карась плывёт в воде.")
+    }
+}
+
+class Seagull : Flyable, Swimmable {
+    override fun fly() {
+        println("Чайка парит в небе.")
+    }
+
+    override fun swim() {
+        println("Чайка скользит по воде.")
+    }
+}
+
+class Duck : Flyable, Swimmable {
+    override fun fly() {
+        println("Утка летит над озером.")
+    }
+
+    override fun swim() {
+        println("Утка плавает и ныряет.")
+    }
+}
 
 fun main() {
+    val carp = Carp()
+    val seagull = Seagull()
+    val duck = Duck()
 
+    // Демонстрация возможностей передвижения
+    carp.swim()
 
+    seagull.fly()
+    seagull.swim()
+
+    duck.fly()
+    duck.swim()
 }
