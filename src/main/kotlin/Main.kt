32c381f8fcfa5ad6de5da1665cf8dc2292f65bfd
ilya <ignislumenhoros@yandex.ru>
2026-08-20 21:06:52 +0300

    KS-16-1  encapsulation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,14 @@
-package org.example
+import kotlin.random.Random
+
+class Dice {
+    private val value: Int = Random.nextInt(1, 7) // случайное число от 1 до 6
+
+    fun showValue() {
+        println("Выпало число: $value")
+    }
+}
 
 fun main() {
-
-
+    val dice = Dice()
+    dice.showValue()
 }
