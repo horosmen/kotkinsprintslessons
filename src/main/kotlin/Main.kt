c9b82efe2f551d27ce35774a2d844348245dc5e0
ilya <ignislumenhoros@yandex.ru>
2026-08-24 21:21:16 +0300

    KS-16-2  private set

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,20 @@
-package org.example
+class Circle(initialRadius: Double) {
+    companion object {
+        private const val PI = 3.14
+    }
 
-fun main() {
+    init {
+        require(initialRadius > 0) { "Радиус должен быть положительным числом" }
+    }
 
+    var radius: Double = initialRadius
+        private set
 
+    fun setRadius(value: Double) {
+        require(value > 0) { "Радиус должен быть положительным числом" }
+        radius = value
+    }
+
+    val circumference: Double get() = 2 * PI * radius
+    val area: Double get() = PI * radius * radius
 }
