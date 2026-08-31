5512b7f48118fde0e417f2a9e8d89f7e516a86cb
ilya <ignislumenhoros@yandex.ru>
2026-08-31 16:02:34 +0300

    KS-17-5 getter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,41 @@
-package org.example
+class User(
+    login: String,
+    password: String
+) {
+
+    var login: String = login
+        set(value) {
+            field = value
+            println("Логин успешно изменён")
+        }
+
+    var password: String = password
+        get() = "*".repeat(field.length)
+        set(value) {
+            println("Вы не можете изменить пароль")
+        }
+}
 
 fun main() {
 
+    val user = User(
+        login = "miron",
+        password = "12345678"
+    )
 
+    // Читаем данные пользователя
+    println("Логин: ${user.login}")
+    println("Пароль: ${user.password}")
+
+    // Пытаемся изменить логин
+    user.login = "miron123"
+
+    // Проверяем новый логин
+    println("Новый логин: ${user.login}")
+
+    // Пытаемся изменить пароль
+    user.password = "qwerty"
+
+    // Проверяем пароль
+    println("Пароль: ${user.password}")
 }
