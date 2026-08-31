a6f0b6839a09849462b6934d49b72ac50ade24b5
ilya <ignislumenhoros@yandex.ru>
2026-08-31 15:58:32 +0300

    KS-17-3 getter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,38 @@
-package org.example
+class Folder(
+    private val folderName: String,
+    private val fileCount: Int,
+    private val isSecret: Boolean
+) {
+
+    val name: String
+        get() {
+            return if (isSecret) {
+                "скрытая папка"
+            } else {
+                folderName
+            }
+        }
+
+    val files: Int
+        get() {
+            return if (isSecret) {
+                0
+            } else {
+                fileCount
+            }
+        }
+}
 
 fun main() {
 
+    // Создаём скрытую папк
+    val secretFolder = Folder(
+        folderName = "Documents",
+        fileCount = 25,
+        isSecret = true
+    )
 
+    // Пытаемся прочитать данные
+    println("Имя папки: ${secretFolder.name}")
+    println("Количество файлов: ${secretFolder.files}")
 }
