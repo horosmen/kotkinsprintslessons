e2a7e5e0f933da1d08b5a01f6b8a58b5c15ee0fb
ilya <ignislumenhoros@yandex.ru>
2026-08-31 15:53:08 +0300

    KS-17-2  custom set

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,35 @@
-package org.example
+class Ship {
+
+    var name: String = "Aurora"
+        set(value) {
+            println("Warning: Ship name cannot be changed!")
+        }
+
+    var averageSpeed: Double = 25.5
+
+    var homePort: String = "Rotterdam"
+}
 
 fun main() {
 
+    val ship = Ship()
 
+    println("Ship name: ${ship.name}")
+    println("Average speed: ${ship.averageSpeed}")
+    println("Home port: ${ship.homePort}")
+
+    // Пытаемся изменить имя
+    ship.name = "Titanic"
+
+    // Проверяем, изменилось ли имя
+    println("Ship name: ${ship.name}")
+
+    // Изменяем скорость
+    ship.averageSpeed = 30.0
+
+    // Изменяем порт
+    ship.homePort = "Amsterdam"
+
+    println("New average speed: ${ship.averageSpeed}")
+    println("New home port: ${ship.homePort}")
 }
