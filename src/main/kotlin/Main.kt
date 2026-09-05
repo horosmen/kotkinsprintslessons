953ee1a5edc5532e96d394d9c5d44498eb2211c1
ilya <ignislumenhoros@yandex.ru>
2026-09-05 21:04:18 +0300

    KS-18-4 when + is

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,37 @@
-abstract class Box(
-    open val width: Double,
-    open val height: Double,
-    open val depth: Double
-) {
-    // Площадь поверхности прямоугольного параллелепипеда: 2*(wh + wd + hd)
+sealed class Box {
+    /** Площадь поверхности коробки */
     abstract fun surfaceArea(): Double
+
+    /** Прямоугольная коробка с тремя измерениями */
+    data class Rectangular(
+        val length: Double,
+        val width: Double,
+        val height: Double
+    ) : Box() {
+        init {
+            require(length > 0 && width > 0 && height > 0) {
+                "Размеры должны быть положительными"
+            }
+        }
+
+        override fun surfaceArea(): Double =
+            2 * (length * width + width * height + height * length)
+    }
+
+    /** Коробка-куб, задаётся только ребром */
+    data class Cube(val side: Double) : Box() {
+        init {
+            require(side > 0) { "Ребро должно быть положительным" }
+        }
+
+        override fun surfaceArea(): Double = 6 * side * side
+    }
 }
 
-class RectangularBox(
-    override val width: Double,
-    override val height: Double,
-    override val depth: Double
-) : Box(width, height, depth) {
+fun main() {
+    val box1: Box = Box.Rectangular(2.0, 3.0, 4.0)
+    val box2: Box = Box.Cube(5.0)
 
-    init {
-        require(width > 0 && height > 0 && depth > 0) { "Размеры должны быть положительными" }
-    }
-
-    override fun surfaceArea(): Double {
-        return 2 * (width * height + width * depth + height * depth)
-    }
-}
-
-class CubeBox(
-    side: Double
-) : Box(side, side, side) {
-
-    init {
-        require(side > 0) { "Длина ребра должна быть положительной" }
-    }
-
-    override fun surfaceArea(): Double {
-        // У куба 6 граней, каждая площадью side^2
-        return 6 * side * side
-    }
+    println("Площадь прямоугольной: ${box1.surfaceArea()}") // 52.0
+    println("Площадь куба: ${box2.surfaceArea()}")         // 150.0
 }
