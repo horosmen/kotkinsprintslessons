bfb571a565e46974e2753d7f788f7c749caff090
ilya <ignislumenhoros@yandex.ru>
2026-09-05 21:00:14 +0300

    KS-18-4 when + is

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,37 @@
-package org.example
+abstract class Box(
+    open val width: Double,
+    open val height: Double,
+    open val depth: Double
+) {
+    // Площадь поверхности прямоугольного параллелепипеда: 2*(wh + wd + hd)
+    abstract fun surfaceArea(): Double
+}
 
-fun main() {
+class RectangularBox(
+    override val width: Double,
+    override val height: Double,
+    override val depth: Double
+) : Box(width, height, depth) {
 
+    init {
+        require(width > 0 && height > 0 && depth > 0) { "Размеры должны быть положительными" }
+    }
 
+    override fun surfaceArea(): Double {
+        return 2 * (width * height + width * depth + height * depth)
+    }
+}
+
+class CubeBox(
+    side: Double
+) : Box(side, side, side) {
+
+    init {
+        require(side > 0) { "Длина ребра должна быть положительной" }
+    }
+
+    override fun surfaceArea(): Double {
+        // У куба 6 граней, каждая площадью side^2
+        return 6 * side * side
+    }
 }
